5fe7d75acaf3390b7282b4b2011b6c941cb5afe7
jaiquire814 <157500113+jaiquire814@users.noreply.github.com>
2024-01-23 11:33:41 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # . .
+    `)
+led.plotBarGraph(
+5,
+3,
+true
+)
+led.unplot(2, 4)
 basic.forever(function () {
 	
 })
