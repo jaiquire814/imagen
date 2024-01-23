b96e74d514c1c3a06a76ed47526adf47d9139b21
jaiquire814 <157500113+jaiquire814@users.noreply.github.com>
2024-01-23 14:27:43 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ for (let index = 0; index < 2; index++) {
     images.iconImage(IconNames.Angry).showImage(4, 30)
     basic.showLeds(`
         # # # # #
-        . . # . .
-        . . # . .
-        . . # . .
-        # # # . .
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
         `)
     basic.showLeds(`
-        # # # # #
         # . . . #
-        # # # # #
-        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
         # . . . #
         `)
 }
