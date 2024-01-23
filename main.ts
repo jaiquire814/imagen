9a02213692d67e77c61c4b9f21edb4733fa1aaa8
jaiquire814 <157500113+jaiquire814@users.noreply.github.com>
2024-01-23 11:57:00 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
-for (let index = 0; index < 4; index++) {
-    images.iconImage(IconNames.Surprised).showImage(4, 30)
-}
-basic.showLeds(`
-    # # # # #
-    . . # . .
-    . . # . .
-    . . # . .
-    # # # . .
-    `)
-led.plotBarGraph(
-3,
-1,
-true
-)
-led.unplot(5, 4)
-for (let index = 0; index < 4; index++) {
-	
+for (let index = 0; index < 2; index++) {
+    images.iconImage(IconNames.Heart).showImage(4, 30)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # . .
+        `)
+    led.plotBarGraph(
+    3,
+    1,
+    true
+    )
+    led.unplot(5, 4)
+    for (let index = 0; index < 4; index++) {
+    	
+    }
 }
 basic.forever(function () {
 	
